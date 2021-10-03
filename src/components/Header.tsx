bd7adef1ae8ae000f5027e5483cf9a8a504726d6
B1ackin <xx13.02@gmail.com>
2021-10-03 15:51:56 +0300

    add style

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/style.css';
+
+export function Header() {
+    return <div className="header">
+    <div className="header-top">
+        <div className="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/VK_Full_Logo.svg" alt=""/>
+        </div>
+        <div className="login">
+            Вход
+        </div>
+    </div>
+    </div>
+}
+
