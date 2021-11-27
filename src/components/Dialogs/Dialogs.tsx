a61796359aa1f6ab689463da61275520b7fe7c4f
B1ackin <xx13.02@gmail.com>
2021-11-27 15:05:05 +0300

    lesson 22 completed

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import s from '../Header/Header.module.css';
+import s from '../Dialogs/Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 export function Dialogs() {
-    return <div >
-        Dialogs
+    return (
+        <div className={s.dialogs}>
+        <div className={s.dialogsItem}>
+            <div className={s.dialog}>
+                 <NavLink to="/dialogs/1">Valera</NavLink>
+            </div>
+            <div className={s.dialog + ' ' + s.active}>
+                <NavLink to="/dialogs/2">Igor</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to="/dialogs/3">Misha</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to="/dialogs/4">Kirill</NavLink>
+            </div>
+        </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi, how are you?
+                </div>
+                <div className={s.message}>
+                    Hi, how are you?
+                </div>
+                <div className={s.message}>
+                    Hi, how are you?
+                </div>
+            </div>
     </div>
+    )
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import s from '../Navbar/Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 export function Navbar() {
     return <div className={s.navbar}>
         <div>
-            <a href="/profile">Profile</a>
+            <NavLink to="/profile">Profile</NavLink>
         </div>
         <div>
-            <a href="/dialogs">Messages</a>
+            <NavLink to="/dialogs">Messages</NavLink>
         </div>
         <div>News</div>
         <div>Music</div>
