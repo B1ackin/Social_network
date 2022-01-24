d3af2795475c0a2bde4a3cc3e6a4350e2ef37ff2
B1ackin <xx13.02@gmail.com>
2022-01-24 11:21:14 +0300

    lesson 54 completed - add componentDidMount

--- src/components/Users/UsersC.tsx
+++ src/components/Users/UsersC.tsx
@@ -13,13 +13,12 @@ type UsersPropsType ={
 
 
 export class UsersC extends React.Component<UsersPropsType>{
-    constructor(props: UsersPropsType) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
-                     props.setUsers(response.data.items)
-               })
 
-    }
+        componentDidMount() {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
 
     render() {
 
